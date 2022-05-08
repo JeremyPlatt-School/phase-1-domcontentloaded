d14caaf9b5a23293b23c0986144673fe7aaf077f
Jeremy Platt <jplatt4427@gmail.com>
2022-05-08 10:20:45 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    changeText();
+  });
+  
+  function changeText(){
+      document.getElementById("text").textContent = "This is really cool!"
+  }
+  
